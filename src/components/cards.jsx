b16b37c77933fdc9ebe9289f5010f23ac9580582
Manas Salian <147305419+Manas-salian@users.noreply.github.com>
2025-03-26 12:33:47 +0530

    added cards and section

--- src/components/cards.jsx
+++ src/components/cards.jsx
@@ -3,12 +3,12 @@ import Card from 'react-bootstrap/Card';
 
 function CardComponent({ title, text, imagesource }) {
     return (
-        <Card style={{ width: '18rem' }}>
+        <Card className="text-white" style={{ width: '18rem' }}>
             <Card.Img variant="top" src={imagesource} />
-            <Card.Body>
+            <Card.ImgOverlay className='d-flex flex-column justify-content-end'>
                 <Card.Title>{title}</Card.Title>
                 <Card.Text>{text}</Card.Text>
-            </Card.Body>
+            </Card.ImgOverlay>
         </Card>
     );
 }
